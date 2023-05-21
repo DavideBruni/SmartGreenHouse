8203df6b5162d9ec0aff85563fbc2d8668dd73cd
Luca372 <60453103+Luca372@users.noreply.github.com>
2023-05-21 22:13:08 +0200

    Add files via upload

---
+++ MQTT_sensors/project-conf.h
@@ -0,0 +1,17 @@
+
+/*---------------------------------------------------------------------------*/
+#ifndef PROJECT_CONF_H_
+#define PROJECT_CONF_H_
+/*---------------------------------------------------------------------------*/
+/* Enable TCP */
+#define UIP_CONF_TCP 1
+
+//#define LOG_CONF_LEVEL_IPV6                        LOG_LEVEL_DBG
+//#define LOG_CONF_LEVEL_RPL                         LOG_LEVEL_DBG
+//#define LOG_CONF_LEVEL_6LOWPAN                     LOG_LEVEL_DBG
+//#define LOG_CONF_LEVEL_TCPIP                       LOG_LEVEL_DBG
+
+/*---------------------------------------------------------------------------*/
+#endif /* PROJECT_CONF_H_ */
+/*---------------------------------------------------------------------------*/
+/** @} */
