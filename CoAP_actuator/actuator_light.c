78d0c0d3eae82e99875ebbd2333ace0fdd77a3a6
DavideBruni <davidebruni.005@outlook.it>
2023-05-23 09:59:31 +0200

    Add light actuator

---
+++ CoAP_actuator/actuator_light.c
@@ -0,0 +1,77 @@
+#include "contiki.h"
+#include "coap-engine.h"
+#include "coap-blocking-api.h"
+#include "net/ipv6/uip.h"
+#include "net/ipv6/uip-ds6.h"
+#include "sys/etimer.h"
+#include "os/dev/leds.h"
+#include <stdio.h>
+
+
+// Server IP and resource path
+#define SERVER_EP "coap://[fd00::1]:5683"
+#define NODE_NAME_JSON "{\"name\":\"actuator_light\"}"
+#define MAX_REGISTRATION_RETRY 3
+
+static coap_endpoint_t server_ep;
+static coap_message_t request[1]; /* This way  the packet can be treated as pointer as usual. */
+static char *service_registration_url = "/registration";
+static int max_registration_retry = MAX_REGISTRATION_RETRY;
+
+void client_chunk_handler(coap_message_t *response){
+	if(response == NULL) {
+		printf("Request timed out\n");
+	}else if(response->code != 65){
+		printf("Error: %d\n",response->code);	
+	}else{
+		printf("OK\n");
+		max_registration_retry = 0;		// if = 0 --> registration ok!
+		return;
+	}
+	
+	// If I'm at this point, there was some problem in the registration phasse, so we decide to try again until max_registration_retry != 0
+	max_registration_retry--;
+	if(max_registration_retry==0)
+		max_registration_retry=-1;
+}
+
+
+
+extern coap_resource_t res_light;
+static struct etimer sleep_timer;
+
+PROCESS(light_thread, "light");
+AUTOSTART_PROCESSES(&light_thread);
+
+PROCESS_THREAD(light_thread, ev, data){
+	
+	PROCESS_BEGIN();
+
+	while(max_registration_retry!=0){
+		/* -------------- REGISTRATION --------------*/
+		// Populate the coap_endpoint_t data structure
+		coap_endpoint_parse(SERVER_EP, strlen(SERVER_EP), &server_ep);
+		// Prepare the message
+		coap_init_message(request, COAP_TYPE_CON,COAP_POST, 0);
+		coap_set_header_uri_path(request, service_registration_url);
+		//Set payload
+		coap_set_payload(request, (uint8_t *)NODE_NAME_JSON, sizeof(NODE_NAME_JSON) - 1);
+	
+		COAP_BLOCKING_REQUEST(&server_ep, request, client_chunk_handler);
+    
+		/* -------------- END REGISTRATION --------------*/
+		if(max_registration_retry == -1){		// something goes wrong more MAX_REGISTRATION_RETRY times, node goes to sleep then try again
+			etimer_set(&sleep_timer, 3000*CLOCK_SECOND);
+			PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&sleep_timer));
+			max_registration_retry = MAX_REGISTRATION_RETRY;
+		}
+	}
+    
+    
+	coap_activate_resource(&res_light, "actuator_window");
+	
+	PROCESS_YIELD();	
+	
+
+PROCESS_END();
+}
---
+++ CoAP_actuator/resources/res-light.c
@@ -0,0 +1,59 @@
+#include "contiki.h"
+#include "coap-engine.h"
+#include "os/dev/leds.h"
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Log configuration */
+#include "sys/log.h"
+#define LOG_MODULE "App"
+#define LOG_LEVEL LOG_LEVEL_APP
+
+static uint8_t light_status = 0; // 0 off, 1 medium brightness, 2 max brightness
+
+static void res_put_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
+
+RESOURCE(res_window,
+         "title=\"SmartGreenHouse: ?acutaor_light=0..\" POST/PUTaction=<action>\";rt=\"Control\";if=\"actuator\"",
+         NULL,
+         NULL,
+         res_put_handler,
+         NULL);
+
+static void res_put_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset){
+    size_t len = 0;
+    const char *action = NULL;
+
+    printf("Sono qui!");
+    if((len = coap_get_post_variable(request, "action", &action))) {
+        if((strncmp(action, "up", len) == 0)){
+            if(light_status == 0){
+                leds_set(LEDS_YELLOW);
+                light_status++;
+            }else if(light_status == 0){
+                leds_set(LEDS_GREEN);               // send respponse back ?
+                light_status++;
+            }else{
+                printf("Light has max brightness"); // send respponse back ?
+            }
+	    }else if((strncmp(action, "down", len) == 0)){
+            if(light_status == 2){
+                leds_set(LEDS_YELLOW);
+                light_status--;
+            }else if(light_status == 1){
+                leds_OFF(LEDS_YELLOW);               // send respponse back ?
+                light_status--;
+            }else{
+                printf("Light is turned off "); // send respponse back ?
+            }
+	    }else if((strncmp(action, "off", len) == 0)){
+            light_status = 0;
+            leds_off(LEDS_NUM_TO_MASK(LEDS_GREEN) | LEDS_NUM_TO_MASK(LEDS_RED) | LEDS_NUM_TO_MASK(LEDS_YELLOW)) 
+	    }else
+            coap_set_status_code(response, BAD_OPTION_4_02);
+    }else{
+        coap_set_status_code(response, BAD_REQUEST_4_00);
+    }
+}
